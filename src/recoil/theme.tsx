4106626b1d9473ade2814d0b82681640675130e3
Huze <dltjrrbs2020@gmail.com>
2023-01-25 01:55:10 +0900

    중복되는 부분 Spread 연산자로 제거 및 Color 세분화 적용

--- src/recoil/theme.tsx
+++ src/recoil/theme.tsx
@@ -1,13 +1,19 @@
 import { DefaultTheme } from 'styled-components';
 
 export const darkTheme: DefaultTheme = {
-  bgColor: '#353b48',
-  textColor: '#f5f6fa',
-  accentColor: '#8c7ae6',
+  mainBgColor: '#353b48',
+  mainTextColor: '#f5f6fa',
+  textColor: {
+    white: '#f5f6fa',
+    gray: '#9E9E9E',
+    light_black: '#404040',
+    black: '#212121',
+  },
+  accentColor: { purple: '#8c7ae6', yellow: '#fbc531', green: '#44bd32' },
 };
 
 export const lightTheme: DefaultTheme = {
-  bgColor: 'whitesmoke',
-  textColor: 'black',
-  accentColor: '#fbc531',
+  ...darkTheme,
+  mainBgColor: 'whitesmoke',
+  mainTextColor: '#212121',
 };
