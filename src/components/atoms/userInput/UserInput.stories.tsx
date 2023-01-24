acd3f69f954a7185eeae44df529ce9a5da3c4274
Huze <dltjrrbs2020@gmail.com>
2023-01-25 01:57:36 +0900

    atom Component인 userInput 추가, typingInput에서 이름 변경

---
+++ src/components/atoms/userInput/UserInput.stories.tsx
@@ -0,0 +1,14 @@
+import { Story, Meta } from '@storybook/react';
+import UserInput, { Props } from './UserInput';
+
+export default {
+  title: 'UserInput',
+  component: UserInput,
+  argTypes: {
+    opacity: { control: 'inline-radio', defaultValue: 1, options: [0, 1] },
+  },
+} as Meta;
+
+const Template: Story<Props> = (args) => <UserInput {...args} />;
+
+export const Default = Template.bind({});
