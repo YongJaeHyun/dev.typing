1b2f9b50d4ad12a8bf02d0ce5de8b8ec7b0e1b75
YongjaeHyun <dltjrrbs2020@gmail.com>
2023-01-26 18:57:25 +0900

    Text가 li 태그로 쓰일 때의 css 정의

--- src/components/atoms/text/Text.tsx
+++ src/components/atoms/text/Text.tsx
@@ -1,5 +1,5 @@
 import { HTMLAttributes } from 'react';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import { fontWeightSwitch } from '../../../utils';
 
 type TFontWeight = 'LIGHT' | 'REGULAR' | 'MEDIUM' | 'BOLD' | 'BOLDEST';
@@ -11,20 +11,32 @@ export interface Props extends HTMLAttributes<HTMLSpanElement> {
   value?: string;
   fontWeight?: TFontWeight;
   fontFamily?: TFontFamily;
+  as?: React.ElementType;
 }
 
 const Span = styled.span<Props>`
   font-size: ${(props) => props.fontSize ?? 16}px;
-  color: ${(props) => props.color ?? props.theme.mainTextColor};
   font-weight: ${({ fontWeight }) =>
     fontWeight !== undefined && fontWeightSwitch[fontWeight]};
   line-height: 1.2;
-  font-family: ${(props) =>
-    props.fontFamily ?? "'Apple SD Gothic Neo' ,'Noto Sans KR', sans-serif"};
+  font-family: ${(props) => props.fontFamily};
+  ${(props) =>
+    props.as === 'li' &&
+    css`
+      height: 20px;
+      line-height: 20px;
+      :before {
+        content: '·';
+        font-size: 25px;
+        vertical-align: middle;
+        color: ${(props) => props.theme.accentColor.themeDefault};
+      }
+    `}
 `;
 
-const Text = (props: Props) => (
+export const Text = (props: Props) => (
   <Span
+    as={props.as}
     fontSize={props.fontSize}
     color={props.color}
     fontWeight={props.fontWeight}
@@ -33,5 +45,3 @@ const Text = (props: Props) => (
     {props.value}
   </Span>
 );
-
-export default Text;
