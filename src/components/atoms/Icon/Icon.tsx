ae4f0254e5ff1c80ce734f8bcfdd8701e9146164
YongjaeHyun <dltjrrbs2020@gmail.com>
2023-01-30 20:05:18 +0900

    theme 토글버튼에서 Icon을 쓸 때에는 isToggleBtn이라는 prop을 통해 css를 분리함. isDark를 prop으로 건네받는 것이 아니라 recoil hook을 통해 상태를 받아오도록함. width, height prop 추가 및 name을 필수 prop으로 변경

--- src/components/atoms/Icon/Icon.tsx
+++ src/components/atoms/Icon/Icon.tsx
@@ -1,36 +1,49 @@
 import styled, { css } from 'styled-components';
+import { useRecoilValue } from 'recoil';
+import { isDarkState } from '../../../recoil/recoilStates';
 
 export interface Props {
+  name: string;
+  width?: string;
+  height?: string;
+  isToggleBtn?: boolean;
   isDark?: boolean;
   color?: string;
   fill?: number;
   size?: number;
-  name?: string;
 }
 
 const Span = styled.span<Props>`
+  width: ${(props) => props.width};
+  height: ${(props) => props.height};
   font-variation-settings: 'FILL' ${(props) => props.fill ?? 0}, 'wght' 500,
     'GRAD' 0, 'opsz' ${(props) => props.size ?? 20};
-  display: inline;
   color: ${(props) => props.color ?? props.theme.textColor.themeDefault};
   ${(props) =>
-    props.isDark !== undefined &&
+    props.isToggleBtn === true &&
     css`
       transform: translate(-50%, -50%);
-      left: ${props.isDark ? '17px' : '59px'};
+      left: ${props.isDark === true ? '17px' : '52px'};
       position: absolute;
       transition: all 0.4s ease-in-out;
     `}
 `;
 
-export const Icon = (props: Props) => (
-  <Span
-    className="material-symbols-rounded"
-    color={props.color}
-    isDark={props.isDark}
-    fill={props.fill}
-    size={props.size}
-  >
-    {props.name}
-  </Span>
-);
+export const Icon = (props: Props) => {
+  const isDark = useRecoilValue(isDarkState);
+  return (
+    <Span
+      className="material-symbols-rounded"
+      color={props.color}
+      fill={props.fill}
+      size={props.size}
+      isToggleBtn={props.isToggleBtn}
+      isDark={isDark}
+      width={props.width}
+      height={props.height}
+      name={props.name}
+    >
+      {props.name}
+    </Span>
+  );
+};
